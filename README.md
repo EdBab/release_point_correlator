534e5d0b50ffac8482351cba804f07aa8f692b31
Eddie Babbe <ebabbe@Eddies-MBP.home>
2023-07-01 13:10:03 -0700

    updated readme to includes images and explanation

--- README.md
+++ README.md
@@ -1,2 +1,52 @@
-# pitch_tunneling_value
-This code examines the release point of pitchers to determine which best effectively "tunnel", and then seeks to conclude whether this is positively correlated with strong performance
+# Release Point Consistency
+This code examines the release point of pitchers to determine which pitchers produce the most consistent release point with their pitchers and then seeks to conclude whether this is positively correlated with strong performance
+
+## Motivation
+It is a commonly held belief in baseball that your mechanics as a pitcher should not telegraph the location of your pitch or pitch type to the batter. Therefore, your release point should be consistent in order to delay the batter recognizing the pitch characteristics for as long as possible. While that makes sense on the surface, is that actually born out in reality? Does a consistent release point correlate to strong performance? I wanted to find that out for myself.
+
+## Methodology
+I relied on the pybaseball api to fetch statcast data from 2015-2022. This gave me access to X and Y coordinates for every pitch thrown by a pitcher during this time frame. From there, I calculated a pitcher's average release point, and then I determined each pitch's distance from that average release point within the 2-D space. Now that each pitch had a corresponding "distance" from the average release point for that pitcher, I averaged the distances together to determine just how consistent that pitcher was. A lower variance means the pitcher is more consistent. 
+
+## Using the tool
+### Installing the dependencies
+```
+pip3 install -r requirements.txt
+```
+
+### Running the program
+```
+python3 release_point_correlation.py --help
+usage: release_point_correlation.py [-h] --test_seasons TEST_SEASONS --plate_appearances PLATE_APPEARANCES --stat {ERA,FIP,xERA,WAR,SO,K/9}
+
+A utility that examines the release position of pitchers and seeks to predict high and low performers based off of consistency
+
+options:
+  -h, --help            show this help message and exit
+  --test_seasons TEST_SEASONS
+                        The future season(s) you want to use to examine a pitcher's release point data ex. 2021 or 2015-2022
+  --plate_appearances PLATE_APPEARANCES
+                        The minimum number of batters a pitcher needs to have faced to be considered
+  --stat {ERA,FIP,WAR,SO,K/9}
+                        The stat you want to compare release point variance to
+```
+```
+python3 release_point_correlation.py --test_season 2015-2022 --plate_appearances 100  --stat FIP
+```
+
+## Results
+Below you can find the results for comparing release point consistency to a pitcher's ERA, FIP, WAR, and K/9. To summarize, there is not a strong correlation between release point consistency and performance for any of the metrics.
+
+![FIP](images/variance_fip.png)
+
+![ERA](images/variance_era.png)
+
+![WAR](images/variance_war.png)
+
+![K](images/variance_k_per_nine.png)
+
+
+Here we see release point variance compared to various metrics. In each one, the Pearson Correlation is extremely weak, implying release point consistency is not a strong indicator of good performance. 
+
+## Summary
+
+While it would have been nice to demonstrate that this was a valuable data point to zero in on, it is also valuable to know examining a pitcher's release point consistency is not critical when determining a pitcher's performance. Additionally, it can shape coaching and development strategies. A repeatable release point in and of itself does not seem to be a worthwhile pursuit. Therefore, it is more advantageous to pursue a pitcher's comfort and optimizing pitch flight characteristics as opposed to what happens as the ball is released. 
--- release_point_correlation.py
+++ release_point_correlation.py
@@ -1,6 +1,4 @@
 #!/usr/bin/env python3
-
-from sklearn.preprocessing import MinMaxScaler
 import re
 import matplotlib.pyplot as plt
 import seaborn as sns
@@ -14,9 +12,18 @@ import sys
 cache.enable()
 
 
+def enforce_years(argument):
+    pattern = re.compile("^([0-9]{4})(-[0-9]{4})?$")
+    if pattern.match(argument):
+        return argument
+    else:
+        raise argparse.ArgumentTypeError(
+            "You must either supply a single season, or multiple, consecutive"
+            " seasons separated by a hyphen.\nEx. 2015-2018"
+        )
+
+
 def get_release_variance(pitcher, season):
-    print(type(pitcher))
-    print(pitcher)
     player_id = playerid_lookup(pitcher["Name"].split()[1], pitcher["Name"].split()[0])
     if len(player_id) == 0:
         return -1
@@ -26,59 +33,53 @@ def get_release_variance(pitcher, season):
             if len(player_id) > 1
             else player_id["key_mlbam"].item()
         )
-        year_pitch_data = statcast_pitcher(
-            f"{str(season)}-01-01", f"{str(season + 1)}-01-01", player_id=true_player_id
-        )
-        #release_var = year_pitch_data["release_pos_x"].var() * year_pitch_data["release_pos_y"].var()
-        
-        # year_pitch_data["distance_from_avg_pitch"] = abs(
-        #     (
-        #         year_pitch_data["release_pos_x"]
-        #         - year_pitch_data["release_pos_x"].mean()
-        #     ).mul(
-        #         year_pitch_data["release_pos_y"]
-        #         - year_pitch_data["release_pos_y"].mean()
-        #     )
-        # )
-        x_series = (year_pitch_data["release_pos_x"]
-                - year_pitch_data["release_pos_x"].mean()) ** 2
-        y_series = (year_pitch_data["release_pos_y"]
-                - year_pitch_data["release_pos_y"].mean()) ** 2
-        year_pitch_data["distance_from_avg_pitch"] = np.sqrt(
-            (
-                x_series
-            ) + (
-                y_series
+        if is_multiple_seasons:
+            year_pitch_data = statcast_pitcher(
+                f"{str(season)[0:4]}-01-01",
+                f"{str(season)[5:9]}-12-31",
+                player_id=true_player_id,
             )
-        )
-    except Exception as e:
+        else:
+            year_pitch_data = statcast_pitcher(
+                f"{str(season)}-01-01",
+                f"{str(season + 1)}-01-01",
+                player_id=true_player_id,
+            )
+        x_series = (
+            year_pitch_data["release_pos_x"] - year_pitch_data["release_pos_x"].mean()
+        ) ** 2
+        y_series = (
+            year_pitch_data["release_pos_y"] - year_pitch_data["release_pos_y"].mean()
+        ) ** 2
+        year_pitch_data["distance_from_avg_pitch"] = np.sqrt((x_series) + (y_series))
+    except Exception:
         return -1
-
 
     return year_pitch_data["distance_from_avg_pitch"].mean()
 
 
 parser = argparse.ArgumentParser(
     description=(
-        "A utility that examines the release pos of pitchers and seeks to predict"
-        " high and low performers based off of consistency"
+        "A utility that examines the release position of pitchers and seeks to"
+        " predict high and low performers based off of consistency"
     )
 )
 
 parser.add_argument(
-    "--test_season",
+    "--test_seasons",
     help=(
-        "The future season you want to use to see if traditional stats or"
-        " expected stats are a better predictor of"
+        "The future season(s) you want to use to examine a pitcher's release"
+        " point data ex. 2021 or 2015-2022"
     ),
-    type=int,
+    type=enforce_years,
     required=True,
 )
 
 parser.add_argument(
     "--plate_appearances",
     help=(
-        "The minimum number of batters a pitcher needs to have faced to be considered"
+        "The minimum number of batters a pitcher needs to have faced to be"
+        " considered"
     ),
     type=int,
     required=True,
@@ -86,34 +87,46 @@ parser.add_argument(
 
 parser.add_argument(
     "--stat",
-    help=("The stat you want to compare release point variance to"),
-    choices=["ERA", "FIP", "xERA", "WAR", "SO", "K/9"],
+    help="The stat you want to compare release point variance to",
+    choices=["ERA", "FIP", "WAR", "SO", "K/9"],
     required=True,
 )
 
 args = parser.parse_args()
-
-
-if int(args.test_season) < 2015:
+is_multiple_seasons = False
+if "-" in args.test_seasons:
+    is_multiple_seasons = True
+first_year = (
+    args.test_seasons if "-" not in args.test_seasons else int(args.test_seasons[0:4])
+)
+if int(first_year) < 2015:
     print(
         "ERROR: Statcast data was introduced in 2015, you cannot search for"
-        " data before this pos\n"
+        " data before this point\n"
     )
     sys.exit(1)
 
-
 try:
-    test_data = pitching_stats(args.test_season, qual=args.plate_appearances)
+    test_data = (
+        pitching_stats(args.test_seasons, qual=args.plate_appearances)
+        if not is_multiple_seasons
+        else pitching_stats(
+            args.test_seasons[0:4],
+            end_season=args.test_seasons[5:9],
+            qual=args.plate_appearances,
+            ind=0,
+        )
+    )
 except Exception:
     print(
-        f"ERROR: Cannot fetch Statcast data for {args.test_season}. Has data"
+        f"ERROR: Cannot fetch Statcast data for {args.test_seasons}. Has data"
         " been published for this year yet?\n"
     )
     sys.exit(1)
 
 
 for index, row in test_data.iterrows():
-    variance = get_release_variance(row, args.test_season)
+    variance = get_release_variance(row, args.test_seasons)
     test_data.at[index, "release_variance"] = variance
 
 
@@ -121,7 +134,10 @@ test_data = test_data[test_data.release_variance != -1]
 
 test_data["release_variance"] = test_data["release_variance"].apply(lambda x: x * 100)
 fig_ba, ax_ba = plt.subplots(1, 1, figsize=(10, 5))
-fig_ba.suptitle(f"Release point variance v. {args.stat} in {args.test_season}")
+
+for i in range(0, 39):
+    print("hey")
+fig_ba.suptitle(f"Release point variance v. {args.stat} in {args.test_seasons}")
 sns.regplot(
     test_data,
     x="release_variance",
@@ -131,6 +147,7 @@ sns.regplot(
     ax=ax_ba,
 )
 ax_ba.set_title(
-    f"Pearson coefficient: {str(round(pd.concat([test_data['release_variance'], test_data[f'{args.stat}']], axis=1).corr().iloc[0,1],2))}"
+    "Pearson coefficient:"
+    f" {str(round(pd.concat([test_data['release_variance'], test_data[f'{args.stat}']], axis=1).corr().iloc[0,1],2))}"
 )
 plt.show()
